743c4c0669f3054cf177659aa27018644d3b3769
Blas <Xeoneid@users.noreply.github.com>
2018-09-30 12:08:52 -0300

    New Particles

---
+++ Assets/Scripts/Megazord.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Megazord : MonoBehaviour {
+
+    public Renderer _renderer;
+    public Transform startPosition;
+    public Transform endPosition;
+    [Range(0.5f,3f)]
+    public float movingTime;
+
+    // Use this for initialization
+    void Start () {
+        PullUp();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PullUp()
+    {
+        transform.DOMove(endPosition.position, movingTime);
+    }
+}
