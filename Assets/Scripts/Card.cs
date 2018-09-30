b6e73b6bc45806d3b9900c8b858bdfa15d79cc09
Blas <Xeoneid@users.noreply.github.com>
2018-09-30 08:29:41 -0300

    Card animation and new lightning

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class Card : MonoBehaviour {
@@ -10,6 +12,22 @@ public class Card : MonoBehaviour {
     public Vector2 bigSize = Vector2.one * 1f;
     public bool isBig = false;
     private SpriteRenderer _renderer;
+    private bool pickedUp = false;
+
+    [Header("Animation")]
+    public bool animate = true;
+    [Range(0.05f,0.5f)]
+    public float offsetFloating;
+    [Range(0.05f, 1f)]
+    public float speeningSpeed = 0.3f;
+    [Range(0.05f, 1f)]
+    public float pickUpSpeed= 0.8f;
+
+    private void OnValidate()
+    {
+        Awake();
+        RenderCard();
+    }
 
     private void Awake()
     {
@@ -18,17 +36,42 @@ public class Card : MonoBehaviour {
 
     public void RenderCard()
     {
-        _renderer.sprite = asset.sprite;
-        _renderer.size = isBig ? bigSize : pickUpSize;
+        gameObject.name = !asset ? "BlankCard" : asset.cardValue.ToString() + "Of" + asset.cardType.ToString();
+        if (asset)
+        {
+            _renderer.enabled = true;
+            _renderer.sprite = asset.sprite;
+            _renderer.gameObject.transform.localScale = isBig ? bigSize : pickUpSize;
+        }
+        else
+            _renderer.enabled = false;
     }
 
     // Use this for initialization
     void Start () {
         RenderCard();
 	}
+
+    public void PickUp(BaseEventData eventData)
+    {
+        if (!pickedUp)
+        {
+            Player.instance.cardsHand.Add(this);
+            transform.DOMove(Player.instance.transform.position, pickUpSpeed);
+            _renderer.DOFade(0, pickUpSpeed);
+            //transform.DOScale(0, pickUpSpeed);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if(animate)
+            Animate();
 	}
+
+    void Animate()
+    {
+        transform.Rotate(Vector3.up * speeningSpeed);
+        //transform.Translate(Vector3.up * Mathf.Sin(Time.deltaTime));
+    }
 }
--- Assets/Scripts/CardAsset.cs
+++ Assets/Scripts/CardAsset.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 [CreateAssetMenu]
 public class CardAsset : ScriptableObject {
-
+    
     public Sprite sprite;
     public CardType cardType;
     public CardValue cardValue;
