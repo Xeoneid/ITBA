f67db4f97939dd81303d1a383f6cac8699c6931b
Blas <Xeoneid@users.noreply.github.com>
2018-10-03 05:00:36 -0300

    Guns scripts

---
+++ Assets/Scripts/Gun/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExtensionMethods;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody _rigidbody { get; private set; }
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    public bool isActive { get; private set; }
+
+    /// <summary>
+    /// Activates/Deactivates bullet GameObject
+    /// </summary>
+    /// <param name="enabled"></param>
+    public void SetActive(bool enabled)
+    {
+        isActive = enabled;
+        gameObject.SetActive(enabled);
+        _rigidbody.velocity = Vector3.zero;
+        if (!enabled)
+            BulletsManager.instance.bulletPool.Push(this);
+    }
+
+    private float timeAlive = 0;
+
+    private void Update()
+    {
+        if (timeAlive >= BulletsManager.BulletsTimeAlive)
+        {
+            timeAlive = 0;
+            SetActive(false);
+        }
+        else
+            timeAlive += Time.deltaTime;
+    }
+}
